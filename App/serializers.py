6ef9ba1f2e42d90237abc3cf7772e1438cd31e08
anujnaRao <anujna.p.rao@gmail.com>
2020-07-24 19:03:56 +0530

    api_view

--- App/serializers.py
+++ App/serializers.py
@@ -5,20 +5,20 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'author', 'email']
-
+        # fields = ['id', 'title', 'author', 'email']
+        fields = '__all__'
         # title = serializers.CharField(max_length=50)
         # author = serializers.CharField(max_length=100)
         # email = serializers.EmailField(max_length=50)
         # date = serializers.DateTimeField()
-        #
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.email = validated_data.get('email', instance.email)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
+    # def create(self, validated_data):
+    #     return Article.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.author = validated_data.get('author', instance.author)
+    #     instance.email = validated_data.get('email', instance.email)
+    #     instance.date = validated_data.get('date', instance.date)
+    #     instance.save()
+    #     return instance
